dd2ea70a4d17eecb7d368fa4d06030d28a35714d
Pascal Precht <pascal.precht@googlemail.com>
2013-02-06 21:49:18 +0100

    First approaches building a provider

--- src/angular.paginator.js
+++ src/angular.paginator.js
@@ -1,1 +1,24 @@
 angular.module('ngPaginator', []);
+
+angular.module('ngPaginator').config(['$provide', function ($provide) {
+
+  $provide.provider('$paginator', function () {
+
+    var defaults = this.defaults = {
+
+    };
+
+    function isPromise(possiblePromise) {
+      return angular.isFunction(possiblePromise.then);
+    }
+
+    this.$get = ['$q', function ($q) {
+
+      function $paginator(config) {
+
+      };
+
+      return $paginator;
+    }];
+  });
+}]);
