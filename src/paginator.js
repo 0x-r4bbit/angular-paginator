c63f2ec2e16f56751ea3e8a56390237f492e51b0
Pascal Precht <pascal.precht@googlemail.com>
2013-05-20 15:51:34 +0200

    Rewrote angular-paginator

--- src/paginator.js
+++ src/paginator.js
@@ -1,130 +1,77 @@
-angular.module('ngPaginator', []);
+angular.module('pascalprecht.paginator', ['ng']);
 
-angular.module('ngPaginator').config(['$provide', function ($provide) {
+angular.module('pascalprecht.paginator').provider('$paginate', function () {
 
-  $PaginatorProvider = function () {
+  var $perPage = 10,
+      $currentPage = 0;
 
-    var $config = {
-      itemsPerPage: 10,
-      currentPage: 0,
-      requestType: 'jsonp'
-    };
 
-    this.itemsPerPage = function (count) {
-      if (count) {
-        $config.itemsPerPage = count;
-      } else {
-        return $config.itemsPerpage;
-      }
-    };
-
-    this.requestType = function (type) {
-      if (type) {
-        $config.requestType = type;
-      } else {
-        return $config.requestType;
-      }
-    };
-
-    this.$get = ['$http', '$q', function ($http, $q) {
-
-      function $paginator(options) {
-        var promise,
-            config = {},
-            usingHttpService = angular.isDefined(options.url);
-
-        config.requestType = options.type || $config.requestType;
-        config.itemsPerPage = options.itemsPerPage || $config.itemsPerPage;
-
-        if (!usingHttpService) {
-          promise = options;
-        } else {
-          promise = $http[config.requestType](options.url);
-        }
-
-        promise = promise.then(function (res) {
-          if (!usingHttpService || !angular.isDefined(options.model)) {
-            return paginate(res, config);
-          }
-          return paginate(res[options.model], config);
-        }, function (reason) {
-          return $q.reject(reason);
-        });
-
-        promise.done = function (fn) {
-          promise.then(function (data) {
-            fn(data);
-          });
-          return promise;
-        };
-
-        promise.error = function (fn) {
-          promise.then(null, function (reason) {
-            fn(reason);
-          });
-          return promise;
-        };
-        return promise;
-      };
-
-      function paginate(dataSet, config) {
-
-        var deferred = $q.defer();
-
-        if (angular.isArray(dataSet)) {
-          var pages = [],
-              len = dataSet.length,
-              i = 0;
-
-          for (; i < len; ++i) {
-            if (i % config.itemsPerPage === 0) {
-              pages[Math.floor(i / config.itemsPerPage)] = [dataSet[i]];
-            } else {
-              pages[Math.floor(i / config.itemsPerPage)].push(dataSet[i]);
-            }
-          }
-
-          deferred.resolve({pages: pages, total: pages.length});
-        } else {
-          deferred.reject({pages: [], total: 0});
-        }
-        return deferred.promise;
-      }
-      return $paginator;
-    }];
-  }
-
-  $provide.provider('$paginator', $PaginatorProvider);
-}]);
-
-angular.module('ngPaginator').directive('paginator', function () {
-  return {
-    restrict: 'E',
-    replace: true,
-    scope: {
-      count: '@',
-      goTo: '&'
-    },
-    template: [
-      '<ul class="paginator">',
-      '<li ng-repeat="n in range(count)"><a ng-click="goTo({page: n+1})">{{n+1}}</a></li>',
-      '</ul>'
-    ].join(''),
-    link: function (scope, element, attrs) {
-
-      scope.range = function (start, end) {
-        var ret = [];
-
-        if (!end) {
-          end = start;
-          start = 0;
-        }
-
-        for (var i = start; i < end; i++) {
-          ret.push(i);
-        }
-        return ret;
-      };
+  this.perPage = function (count) {
+    if (!count) {
+      return $perPage;
     }
+    $perPage = count;
   };
+
+  this.$get = ['$q', function ($q) {
+
+    var $paginate = function (someThing, options) {
+
+      var config = {},
+          deferred = $q.defer();
+
+      if (options) {
+        config.perPage = options.perPage || $perPage;
+      } else {
+        config.perPage = $perPage
+      }
+
+      $q.when(someThing).then(function (data) {
+        paginate(data, config).then(function (paginatorObj) {
+          deferred.resolve(paginatorObj);
+        }, function (paginatorObj) {
+          deferred.reject(paginatorObj);
+        });
+      });
+
+      return deferred.promise;
+    };
+
+    function paginate(dataSet, config) {
+
+      var deferred = $q.defer();
+
+      if (angular.isArray(dataSet)) {
+        var pages = [],
+            len = dataSet.length,
+            i = 0;
+
+        for (; i < len; ++i) {
+          if (i % config.perPage === 0) {
+            pages[Math.floor(i / config.perPage)] = [dataSet[i]];
+          } else {
+            pages[Math.floor(i / config.perPage)].push(dataSet[i]);
+          }
+        }
+
+        deferred.resolve({
+          pages: pages, 
+          total: pages.length,
+          perPage: config.perPage,
+          current: config.current
+        });
+      } else {
+        deferred.reject({
+          pages: [], 
+          total: 0, 
+          perPage: config.perPage, 
+          current: config.current
+        });
+      }
+      return deferred.promise;
+    }
+
+    return $paginate;
+  }];
+
 });
--- test/unit/paginatorSpec.js
+++ test/unit/paginatorSpec.js
@@ -1,1 +1,96 @@
+describe('pascalprecht.paginator', function () {
 
+  beforeEach(module('pascalprecht.paginator'));
+
+  it('should have $paginate service', function () {
+    inject(function ($paginate) {
+      expect($paginate).toBeDefined();
+    });
+  });
+
+  describe('$paginate', function () {
+
+    it('should be a function', function () {
+      inject(function ($paginate) {
+        expect(typeof $paginate).toBe('function');
+      });
+    });
+
+    it('should return object', function () {
+      inject(function ($paginate) {
+        var items = [{},{},{},{}];
+
+        $paginate(items).then(function (data) {
+          expect(typeof data).toBe('object');
+        });
+      });
+    });
+
+    it('should return paginator data object', function () {
+      inject(function ($paginate) {
+        var items = [{},{},{},{}];
+
+        $paginate(items).then(function (data) {
+          expect(data.pages).toBeDefined();
+          expect(data.total).toBeDefined();
+          expect(data.current).toBeDefined();
+          exoect(data.perPage).toBeDefined();
+        });
+      });
+    });
+
+    describe('paginatorObject#pages', function () {
+
+      it('should be an array', function () {
+        inject(function ($paginate) {
+          var items = [{},{},{},{}];
+
+          $paginate(items).then(function (data) {
+            expect(typeof data.pages).toBe('array');
+          });
+        });
+      });
+
+    });
+
+    describe('paginatorObject#current', function () {
+
+      it('should be a number', function () {
+        inject(function ($paginate) {
+          var items = [{},{},{},{}];
+
+          $paginate(items).then(function (data) {
+            expect(typeof data.current).toBe('number');
+          });
+        });
+      });
+    });
+
+    describe('paginatorObject#perPage', function () {
+
+      it('should be a number', function () {
+        inject(function ($paginate) {
+          var items = [{},{},{},{}];
+
+          $paginate(items).then(function (data) {
+            expect(typeof data.perPage).toBe('number');
+          });
+        });
+      });
+    });
+
+    describe('paginatorObject#total', function () {
+
+      it('should be a number', function () {
+        inject(function ($paginate) {
+          var items = [{},{},{},{}];
+
+          $paginate(items).then(function (data) {
+            expect(typeof data.total).toBe('number');
+          });
+        });
+      });
+
+    });
+  });
+});
