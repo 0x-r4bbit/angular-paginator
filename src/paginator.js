0e000e0ec560933f9c705a9ba67c3207cc4f9d5a
yahyaKacem <fuj.tyoli@gmail.com>
2013-06-05 18:07:04 +0100

    correcting the karma test error: 'pascalprecht.paginator  should return paginator data object FAILED'

--- src/paginator.js
+++ src/paginator.js
@@ -1,65 +1,49 @@
-angular.module('pascalprecht.paginator', ['ng']);
-
-angular.module('pascalprecht.paginator').provider('$paginate', function () {
-
+var app = angular.module('pascalprecht.paginator', ['ng']);
+app.provider('$paginate', function () {
   var $perPage = 10;
-
   this.perPage = function (count) {
-    if (!count) {
+    if(!count) {
       return $perPage;
     }
     $perPage = count;
   };
-
-  this.$get = ['$q', function ($q) {
-
+  this.$get = ['$q', function($q) {
     var $paginate = function (someThing, options) {
-
       var config = {},
-          deferred = $q.defer();
-
-      if (options) {
+      deferred   = $q.defer();
+      if(options) {
         config.perPage = options.perPage || $perPage;
-      } else {
+      }else{
         config.perPage = $perPage
       }
-
       $q.when(someThing).then(function (data) {
-
         paginate(data, config).then(function (paginatorObj) {
           deferred.resolve(paginatorObj);
         }, function (paginatorObj) {
           deferred.reject(paginatorObj);
         });
       });
-
       return deferred.promise;
     };
-
     function paginate(dataSet, config) {
-
       var deferred = $q.defer();
-
-      if (angular.isArray(dataSet)) {
+      if(angular.isArray(dataSet)) {
         var pages = [],
-            len = dataSet.length,
-            i = 0;
-
-        for (; i < len; ++i) {
-          if (i % config.perPage === 0) {
+        len       = dataSet.length;
+        for(var i = 0; i < len; ++i) {
+          if(i % config.perPage === 0) {
             pages[Math.floor(i / config.perPage)] = [dataSet[i]];
-          } else {
+          }else{
             pages[Math.floor(i / config.perPage)].push(dataSet[i]);
           }
         }
-
         deferred.resolve({
           pages: pages, 
           total: pages.length,
           perPage: config.perPage,
           current: config.current
         });
-      } else {
+      }else{
         deferred.reject({
           pages: [], 
           total: 0, 
@@ -69,8 +53,6 @@ angular.module('pascalprecht.paginator').provider('$paginate', function () {
       }
       return deferred.promise;
     }
-
     return $paginate;
   }];
-
 });
